b2d342f84b3b7798f7e87c34cbe89587f694e533
claire <claire.quesnel@hotmail.com>
2017-10-07 18:49:03 -0400

    check path

--- LHGames/Strategy.cs
+++ LHGames/Strategy.cs
@@ -77,7 +77,34 @@ namespace LHGames
                     }
                 }
             }
+            return exploreAction(map,gameInfo);
+        }
+        private HighAction exploreAction(Map map, GameInfo gameInfo)
+        {
+            Point player = gameInfo.Player.Position;
+            for (int edge = 1; edge < map.tileTypeMap.GetLength(0); edge++)
+            {
+                for (int i = player.X - edge; i <= player.X + edge && i >= 0 && i < map.tileTypeMap.GetLength(0); i++)
+                {
+                    for (int j = player.Y - edge; j <= player.Y + edge && j >= 0 && j < map.tileTypeMap.GetLength(1); j++)
+                    {
+                        if (map.tileTypeMap[i, j] == TileType.U)
+                        {
+                            Point target = new Point(i, j);
+                            if (Point.DistanceManhatan(target, gameInfo.Player.Position) <= 1)
+                            {
+                                return new Collect(gameInfo, target);
+                            }
+                            else
+                            {
+                                return new Actions.Move(gameInfo, map, target);
+                            }
+                        }
+                    }
+                }
+            }
             return null;
         }
+
     }
 }
